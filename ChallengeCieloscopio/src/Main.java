e2ae2f93fc8d380a9c84c84ffff01f3789c11fc4
MiguelC29 <miguelito28c@gmail.com>
2024-06-26 17:30:22 -0500

    showing menu and showing results

--- ChallengeCieloscopio/src/Main.java
+++ ChallengeCieloscopio/src/Main.java
@@ -1,27 +1,105 @@
 import helpers.APIConsult;
 import models.City;
 import models.CityOmbd;
+import models.Weather;
+import models.WeatherOmbd;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Bienvenido");
+        String menu = """
+                \nChallenge Cieloscopio
+                --------------------------------------------------------
+                Elige una ciudad para obtener los datos meteorológicos:
+                1. Ciudad de México
+                2. Buenos Aires
+                3. Bogotá
+                4. Lima
+                5. Santiago
+                6. Desea consultar otra ciudad
+                7. Salir
+                --------------------------------------------------------""";
+        int option;
 
-        System.out.println("Escribe el nombre de la ciudad a consultar: ");
-        var cityName = teclado.nextLine();
+        System.out.println("Bienvenido al CielosCopio");
 
-        APIConsult apiConsult = new APIConsult();
-        CityOmbd cityOmbd = apiConsult.getCityByName(cityName);
+        do {
+            System.out.println(menu);
+            System.out.println("Ingrese una opción del menú");
+            option = teclado.nextInt();
+            teclado.nextLine();
 
-        if (cityOmbd != null) {
-            City myCity = new City(cityOmbd);
-            System.out.println(myCity);
-        } else {
-            System.out.println("No se pudo obtener la información de la ciudad.");
-        }
+            String cityName = "";
+            boolean validOption;
 
+            switch (option) {
+                case 1:
+                    cityName = "Ciudad de México";
+                    validOption = true;
+                    break;
+                case 2:
+                    cityName = "Buenos Aires";
+                    validOption = true;
+                    break;
+                case 3:
+                    cityName = "Bogotá";
+                    validOption = true;
+                    break;
+                case 4:
+                    cityName = "Lima";
+                    validOption = true;
+                    break;
+                case 5:
+                    cityName = "Santiago";
+                    validOption = true;
+                    break;
+                case 6:
+                    System.out.println("Escribe el nombre de la ciudad a consultar: ");
+                    cityName = teclado.nextLine();
+                    validOption = true;
+                    break;
+                case 7:
+                    System.out.println("Gracias por usar el CielosCopio");
+                    validOption = false;
+                    break;
+                default:
+                    System.out.println("Opción incorrecta, por favor ingrese una opción válida");
+                    validOption = false;
+            }
 
+            if (validOption && !cityName.isEmpty()) {
+                APIConsult apiConsult = new APIConsult();
+                CityOmbd cityOmbd = apiConsult.getCityByName(cityName);
+
+                if (cityOmbd != null) {
+                    City myCity = new City(cityOmbd);
+                    System.out.println(myCity);
+
+                    WeatherOmbd weatherOmbd = apiConsult.getCityByCoords(myCity.getLatitude(), myCity.getLongitude());
+                    Weather weather = new Weather(weatherOmbd);
+
+                    showResults(myCity.getName(), weather);
+                } else {
+                    System.out.println("No se pudo obtener la información de la ciudad.");
+                }
+            }
+
+        } while (option != 7);
+    }
+
+    public static void showResults(String cityName, Weather weather) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Respuesta:");
+        System.out.println("Ciudad: " + cityName);
+        System.out.println("Fecha: " + LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        System.out.println("Horario: " + LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm")) + "\n");
+
+        weather.showWeatherData();
+        System.out.println("---------------------------------------------------------");
     }
 }
