374e71287df3fd86b282b178123360645b0b23ff
MiguelC29 <miguelito28c@gmail.com>
2024-06-27 18:47:25 -0500

    adding precipitacion if it's raining

--- ChallengeCieloscopio/src/models/Weather.java
+++ ChallengeCieloscopio/src/models/Weather.java
@@ -1,23 +1,35 @@
 package models;
 
 public class Weather {
-    private String currentTemperature;
-    private String weatherCondition;
-    private String maxTemperature;
-    private String minTemperature;
+    private final String currentTemperature;
+    private final String weatherCondition;
+    private final String maxTemperature;
+    private final String minTemperature;
+    private String precipitation;
 
     public Weather(WeatherOmbd weatherOmbd) {
         this.currentTemperature = weatherOmbd.main().get("temp");
         this.weatherCondition = weatherOmbd.weather().get(0).get("description");
         this.maxTemperature = weatherOmbd.main().get("temp_max");
         this.minTemperature = weatherOmbd.main().get("temp_min");
+
+        if (weatherOmbd.rain() == null) {
+            this.precipitation = "Valor no informado";
+        } else {
+            if (!weatherOmbd.rain().get("1h").isEmpty()) {
+                this.precipitation = weatherOmbd.rain().get("1h");
+            } else if (!weatherOmbd.rain().get("3h").isEmpty()) {
+                this.precipitation = weatherOmbd.rain().get("3h");
+            }
+        }
     }
 
     public void showWeatherData() {
         System.out.println("Temperatura actual: " + currentTemperature + "°C");
         System.out.println("Condición Climática: " + weatherCondition);
-        System.out.println("");
+        System.out.println();
         System.out.println("Temperatura mínima: " + minTemperature + "°C");
         System.out.println("Temperatura máxima: " + maxTemperature + "°C");
+        System.out.println("Precipitación: " + precipitation);
     }
 }
--- ChallengeCieloscopio/src/models/WeatherOmbd.java
+++ ChallengeCieloscopio/src/models/WeatherOmbd.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 public record WeatherOmbd(
         List<Map<String, String>> weather,
-        Map<String, String> main
+        Map<String, String> main,
+        Map<String, String> rain
 ) {
 }
