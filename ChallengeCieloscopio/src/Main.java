aa094566b78d4be961d8f9e3e83e9f4b9b3e1a68
MiguelC29 <miguelito28c@gmail.com>
2024-07-04 00:44:53 -0500

    solving error handling on not finding a city

--- ChallengeCieloscopio/src/Main.java
+++ ChallengeCieloscopio/src/Main.java
@@ -31,9 +31,9 @@ public class Main {
             String cityName = getCityName(option);
 
             // Realizar la consulta si se obtuvo un nombre de ciudad válido
-            if (cityName != null) {
-                makeQuery(cityName,false); // Realizar consulta sin predicción
+            boolean isValidQuery = makeQuery(cityName,false); // Realizar consulta sin predicción
 
+            if (isValidQuery) {
                 int optionPrediction = getPredictionOption(); // Obtener la opción de predicción del usuario
 
                 // Si la opción es 1, mostrar predicción del clima
@@ -92,27 +92,31 @@ public class Main {
 
     // Método para obtener el nombre de la ciudad según la opción seleccionada
     private static String getCityName(int option) {
+        String cityName = "";
         // Verifica si la opción está en el rango de las opciones predefinidas (1 a 11)
         if (option >= 1 && option <= 11) {
-            return cities[option -1]; // Retorna el nombre de la ciudad correspondiente a la opción seleccionada
+            cityName = cities[option -1]; // Retorna el nombre de la ciudad correspondiente a la opción seleccionada
         } else if (option == 12) {     // Si la opción es 12, se pide al usuario que ingrese el nombre de una ciudad
-            System.out.println("Escriba el nombre de una ciudad: ");
-            return input.nextLine(); // Lee y retorna el nombre de la ciudad ingresada por el usuario
-        } else {
-            // Retorna una cadena vacía indicando que la opción no es válida
-            return "";
+            do {
+                System.out.println("Escriba el nombre de una ciudad: ");
+                cityName = input.nextLine(); // Retorna el nombre de la ciudad ingresada por teclado
+            } while (cityName.isEmpty());
         }
+        return cityName;
     }
 
     // Método para realizar la consulta a la API y mostrar los resultados
-    private static void makeQuery(String cityName, boolean givePrediction) {
+    private static boolean makeQuery(String cityName, boolean givePrediction) {
+        boolean foundCity;
         try {
             APIConsult apiConsult = new APIConsult();
+            CityOmbd cityOmbd;
 
             // Obtener información de la ciudad por nombre
-            CityOmbd cityOmbd = apiConsult.getCoordsByCityName(cityName);
-            City myCity = new City(cityOmbd);
+            cityOmbd = apiConsult.getCoordsByCityName(cityName);
+            foundCity = true;
 
+            City myCity = new City(cityOmbd);
             // Obtener el clima de la ciudad utilizando las coordenadas de la ciudad
             WeatherOmbd weatherOmbd = apiConsult.getWeatherByCoords(myCity.getLatitude(), myCity.getLongitude());
             Weather weather = new Weather(weatherOmbd);
@@ -126,7 +130,9 @@ public class Main {
         } catch (RuntimeException e) {
             // Manejar errores y mostrar mensaje
             System.err.println(e.getMessage());
+            foundCity = false;
         }
+        return foundCity;
     }
 
     // Método para mostrar los resultados del clima
