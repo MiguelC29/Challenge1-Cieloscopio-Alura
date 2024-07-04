c98172db6711f5878717ee15dec68cafaac2136c
MiguelC29 <miguelito28c@gmail.com>
2024-07-03 21:43:12 -0500

    creation of WeatherPredictionOmbd and method to show its data

--- ChallengeCieloscopio/src/models/Weather.java
+++ ChallengeCieloscopio/src/models/Weather.java
@@ -10,6 +10,7 @@ public class Weather {
     private final String atmosphericPressure;
     private final String wind;
     private String precipitation;
+    private String datePrediction;
 
     public Weather(WeatherOmbd weatherOmbd) {
         this.currentTemperature = weatherOmbd.main().get("temp");
@@ -25,26 +26,42 @@ public class Weather {
         if (weatherOmbd.rain() == null) {
             this.precipitation = "Valor no informado";
         } else {
-            if (!weatherOmbd.rain().get("1h").isEmpty()) {
+            if (weatherOmbd.rain().get("1h") != null) {
                 this.precipitation = weatherOmbd.rain().get("1h") + " mm";
-            } else if (!weatherOmbd.rain().get("3h").isEmpty()) {
+            } else if (weatherOmbd.rain().get("3h") != null) {
                 this.precipitation = weatherOmbd.rain().get("3h") + " mm";
             }
+        }
+
+        //Date Prediction
+        if (weatherOmbd.dt_txt() != null) {
+            this.datePrediction = weatherOmbd.dt_txt();
         }
     }
 
     public void showWeatherData() {
         System.out.println("Temperatura actual: " + currentTemperature + "°C");
-        System.out.println("Condición climática: " + weatherCondition.toUpperCase().charAt(0) + weatherCondition.substring(1) + "\n");
-
+        System.out.println("Condición climática: " + weatherCondition.toUpperCase().charAt(0) + weatherCondition.substring(1));
+        System.out.println("+--------------------------------------------------------+");
         System.out.println("Temperatura mínima: " + minTemperature + "°C");
         System.out.println("Temperatura máxima: " + maxTemperature + "°C");
-        System.out.println("Sensación térmica: " + thermalSensation + "°C\n");
-
+        System.out.println("Sensación térmica: " + thermalSensation + "°C");
+        System.out.println("+--------------------------------------------------------+");
         System.out.println("Humedad: " + humidity + "%");
-        System.out.println("Presión atmosférica: " + atmosphericPressure + " hPa\n");
-
+        System.out.println("Presión atmosférica: " + atmosphericPressure + " hPa");
+        System.out.println("+--------------------------------------------------------+");
         System.out.println("Viento: " + wind + " m/s");
         System.out.println("Precipitación: " + precipitation);
+    }
+
+    public void showWeatherPredictionData() {
+        System.out.println("Fecha: " + datePrediction.substring(0, 10));
+        System.out.println("Condición climática: " + weatherCondition.toUpperCase().charAt(0) + weatherCondition.substring(1) + "\n");
+
+        System.out.println("Temperatura: " + currentTemperature + "°C");
+        System.out.println("Temperatura mínima: " + minTemperature + "°C");
+        System.out.println("Temperatura máxima: " + maxTemperature + "°C");
+        System.out.println("Sensación térmica: " + thermalSensation + "°C");
+        System.out.println("+--------------------------------------------------------+");
     }
 }
